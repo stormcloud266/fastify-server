75cd6a583a1e9ae9892ff350d49655492c95b24d
Tawnee <allprt89@gmail.com>
2023-05-03 16:26:40 -0600

    test function

--- functions/index.ts
+++ functions/index.ts
@@ -27,40 +27,49 @@ export default async function (
   done
 ) {
   instance.get("/", async (req: FastifyRequest, res: FastifyReply) => {
-    res.status(200).send({
-      hello: "World",
-    });
+    if (!process.env.WEATHER_API_KEY) {
+      return res.status(500).send("Missing API key");
+    }
+
+    const url = new URL("https://api.openweathermap.org/data/3.0/onecall");
+    url.searchParams.set("lat", "44.526340");
+    url.searchParams.set("lon", "-109.056534");
+    url.searchParams.set("appid", process.env.WEATHER_API_KEY);
+
+    const results = await fetch(url);
+
+    res.status(200).send(results);
   });
 
-  instance.register(
-    async (instance: FastifyInstance, opts: FastifyServerOptions, done) => {
-      instance.get(
-        "/",
-        async (
-          req: FastifyRequest<CustomRouteGenericQuery>,
-          res: FastifyReply
-        ) => {
-          const { name = "" } = req.query;
-          res.status(200).send(`Hello ${name}`);
-        }
-      );
+  // instance.register(
+  //   async (instance: FastifyInstance, opts: FastifyServerOptions, done) => {
+  //     instance.get(
+  //       "/",
+  //       async (
+  //         req: FastifyRequest<CustomRouteGenericQuery>,
+  //         res: FastifyReply
+  //       ) => {
+  //         const { name = "" } = req.query;
+  //         res.status(200).send(`Hello ${name}`);
+  //       }
+  //     );
 
-      instance.get(
-        "/:name",
-        async (
-          req: FastifyRequest<CustomRouteGenericParam>,
-          res: FastifyReply
-        ) => {
-          const { name = "" } = req.params;
-          res.status(200).send(`Hello ${name}`);
-        }
-      );
-      done();
-    },
-    {
-      prefix: "/hello",
-    }
-  );
+  //     instance.get(
+  //       "/:name",
+  //       async (
+  //         req: FastifyRequest<CustomRouteGenericParam>,
+  //         res: FastifyReply
+  //       ) => {
+  //         const { name = "" } = req.params;
+  //         res.status(200).send(`Hello ${name}`);
+  //       }
+  //     );
+  //     done();
+  //   },
+  //   {
+  //     prefix: "/hello",
+  //   }
+  // );
 
   done();
 }
